26adaf312aa3f19f09c468b3e2c91d0079245c6f
Raushan-Shahi <raushanshahi2001@gmail.com>
2023-09-19 14:06:34 +0530

    day 111

---
+++ day111.cpp
@@ -0,0 +1,24 @@
+				// 😉😉😉😉Please upvote if it helps 😉😉😉😉
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        int ind = 0;
+		
+		// sort the vector
+        sort(nums.begin(),nums.end());
+        for(int i = 0; i < nums.size() - 1; i++)
+        {
+			// if two consecutive elements are equal
+			// you have find a duplicate
+			// break the loop
+            if(nums[i] == nums[i+1])
+            {
+                ind = nums[i];
+                break;
+            }
+        }
+        // return duplicate value
+        return ind;
+    }
+	// for github repository link go to my profile.
+};
