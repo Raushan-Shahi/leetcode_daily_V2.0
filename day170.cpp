3f16493f94d668d15b9d537e306612d0bee30258
Raushan-Shahi <raushanshahi2001@gmail.com>
2023-11-17 23:24:37 +0530

    day 170

---
+++ day170.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int minPairSum(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        
+        int minMaxSum = 0;
+        for (int i = 0; i < nums.size() / 2; i++) {
+            minMaxSum = max(minMaxSum, nums[i] + nums[nums.size() - 1 - i]);
+        }
+        
+        return minMaxSum;
+    }
+};class Solution {
+public:
+    int minPairSum(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        
+        int minMaxSum = 0;
+        for (int i = 0; i < nums.size() / 2; i++) {
+            minMaxSum = max(minMaxSum, nums[i] + nums[nums.size() - 1 - i]);
+        }
+        
+        return minMaxSum;
+    }
+};
